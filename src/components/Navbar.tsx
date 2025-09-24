60d55b88cdb284a2712cf6ec38385cdd3b2a9e8b
Corbyn Thomas <corbyn915@gmail.com>
2025-09-23 20:12:08 -0500

    [FIX]added part of the sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,28 @@
-import {} from 'react'
+import { useState } from 'react'
 import { Link } from '@tanstack/react-router'
 import * as Lucide from "lucide-react";
 
 function Navbar() {
+  const[sidebar, setSidebar] = useState(false)
+
+  const showSidebar = () => setSidebar(!sidebar)
   return (
     <>
         <div className="navbar">
             <Link to="/" className='menu-bars'>
-                <Lucide.Menu size="2em"/>
+                <Lucide.Menu size="2em" onClick={showSidebar}/> 
+                {/* I have no idea how to do links */}
             </Link>
         </div>
+        <nav  className={sidebar ? 'nav-menu-active' : "nav-menu" }>
+          <ul className='nav-menu-items'>
+            <li className='navbar-toggle'>
+              <Link to="/" className='menu-bars'>
+                <Lucide.X size="2em"/> 
+              </Link>
+            </li>
+          </ul>
+        </nav>
     </>
   )
 }
