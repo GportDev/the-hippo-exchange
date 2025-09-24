d4e045ac7078bf4465bdf9d84b0c281e337bc5d0
Corbyn Thomas <corbyn915@gmail.com>
2025-09-24 13:54:30 -0500

    [FIX]changed raw css to be tailwind and made it look a bit Prettier

--- src/components/AssetCard.tsx
+++ src/components/AssetCard.tsx
@@ -1,28 +1,24 @@
 import {} from 'react'
 import { Link } from '@tanstack/react-router'
-import "./Card.css"
 
 type AssetCardProps = {
-    imgSrc: string
-    imgAlt: string
-    name: string
-    description: string
-    detailed: string
+  imgSrc: string
+  imgAlt: string
+  name: string
+  detailed: string
 }
 
 const AssetCard: React.FC<AssetCardProps> = ({
   imgSrc,
   imgAlt,
   name,
-  description,
   detailed,
 }) => {
   return (
-    <Link to={detailed} className='card-expand'>
-      <div className='card-containter'>
-        {(imgSrc && imgAlt && <img src={imgSrc} alt={imgAlt} className='card-img' />)}
-        {name && <h1 className='card-name'>{name}</h1>}
-        {description && <p className='card-description'>{description}</p>}
+    <Link to={detailed} className='box-border w-60 rounded-2x'>
+      <div className="box-border font-sans antialiased w-60 rounded-2xl shadow-[0px_10px_8px_#1b1919] flex flex-col m-2 bg-[rgba(61,60,82,1)] h-fit justify-center">
+        {(imgSrc && imgAlt && <img src={imgSrc} alt={imgAlt} className='w-full rounded-2xl'/>)}
+        {name && <h1 className='mx-[5%] my-2 text-2xl font-bold bg-gradient-to-r from-[#FEEF4C] to-white bg-clip-text text-transparent'>{name}</h1>}
       </div>
     </Link>
   )
