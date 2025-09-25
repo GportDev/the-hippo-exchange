0faf9f0069ed4975cd94bc03811589edca49099d
Gabriel Porteiro <71458971+GportDev@users.noreply.github.com>
2025-09-24 22:08:17 -0500

    [FIX] Fix bug reported by Cursor Bot

--- src/components/AssetCard.tsx
+++ src/components/AssetCard.tsx
@@ -15,7 +15,7 @@ const AssetCard: React.FC<AssetCardProps> = ({
   detailed,
 }) => {
   return (
-    <Link to={detailed} className='box-border w-60 rounded-2x'>
+    <Link to={detailed} className='box-border w-60 rounded-2xl'>
       <div className="box-border font-sans antialiased w-60 rounded-2xl shadow-[0px_10px_8px_#1b1919] flex flex-col m-2 bg-[rgba(61,60,82,1)] h-fit justify-center">
         {(imgSrc && imgAlt && <img src={imgSrc} alt={imgAlt} className='w-full rounded-2xl'/>)}
         {name && <h1 className='mx-[5%] my-2 text-2xl font-bold bg-gradient-to-r from-[#FEEF4C] to-white bg-clip-text text-transparent'>{name}</h1>}
