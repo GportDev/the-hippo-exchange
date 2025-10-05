83c739656f4809f152e598b08732f87a6376e0fe
AldenPounders <157765111+AldenPounders@users.noreply.github.com>
2025-10-05 16:25:32 -0500

    Feat/home page pt1 (#20)
    * routing for home page, currently working on contnents
    
    * fixed the bug where text would appear before the sidebar. added a 100
    millisecond delay to text appearance.
    
    * added upcoming maintenance for home page, currently set up like
    maintenance page with static info
    
    * moved the home button to top of navbar# Please enter the commit message for your changes. Lines starting
    
    ---------
    
    Co-authored-by: Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -46,6 +46,18 @@ function Navbar({ isExpanded, onToggle }: NavbarProps) {
             
             {/* Navigation links */}
             <ul className="flex-1 mt-4">
+              <li className='mb-2'>
+                <Link 
+                  to="/home"
+                  className={`flex items-center gap-3 px-4 py-3 text-primary-yellow hover:bg-primary-yellow hover:text-primary-gray transition-colors duration-200 ${
+                    isExpanded ? 'justify-start' : 'justify-center'
+                  }`}
+                  title={!isExpanded ? "My Assets" : undefined}
+                >
+                  <Lucide.House size="1.2em"/>
+                  {showText && <span>Home</span>}
+                </Link>
+              </li>
               <li className="mb-2">
                 <Link 
                   to="/assets/my-assets"
@@ -70,18 +82,7 @@ function Navbar({ isExpanded, onToggle }: NavbarProps) {
                   {showText && <span>Maintenance</span>}
                 </Link>
               </li>
-              <li className='mb-2'>
-                <Link 
-                  to="/home"
-                  className={`flex items-center gap-3 px-4 py-3 text-primary-yellow hover:bg-primary-yellow hover:text-primary-gray transition-colors duration-200 ${
-                    isExpanded ? 'justify-start' : 'justify-center'
-                  }`}
-                  title={!isExpanded ? "My Assets" : undefined}
-                >
-                  <Lucide.House size="1.2em"/>
-                  {showText && <span>Home</span>}
-                </Link>
-              </li>
+              
             </ul>
           </div>
         </nav>
