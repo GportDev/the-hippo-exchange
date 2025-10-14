f75baccf9cb0600b212f33e437677732d2a9163c
TaylorTurnerIT <tjturner44@tntech.edu>
2025-10-14 11:05:25 -0500

    Not quite fixed verify button formatting.

--- src/routes/sign-up/index.tsx
+++ src/routes/sign-up/index.tsx
@@ -179,7 +179,7 @@ function SignUpComponent() {
                 <p className="text-red-500 text-xs min-h-[1.25rem] py-3">{clerkErrors.code || '\u00A0'}</p>
 
                 <div>
-                  <Button type="submit" className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  <Button type="submit" className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Verify
                   </Button>
                 </div>
